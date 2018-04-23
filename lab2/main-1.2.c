e418d2c5f316f271dacb00dac188c8e349fd79de
GabrielMaz <gabrielmaz114@gmail.com>
2018-04-23 00:51:16 -0300

    Perform create event and display event list

--- lab2/main-1.2.c
+++ lab2/main-1.2.c
@@ -14,9 +14,9 @@ enum STATE {
 };
 
 typedef struct Event {
-    int id;
-    char command;
-    char param;
+    int array_postion;
+    char name;
+    char leds;
     unsigned long time;
 };
 
@@ -43,17 +43,23 @@ unsigned long getRtcTime();
 void displayHourUI(unsigned long time_in_sec);
 void inputHourUI();
 cofunc void setDate();
-cofunc void inputHourUI();
 cofunc void askTimeHourData(int ask_time, int ask_date);
 cofunc void askForDateTime(unsigned long* time_in_sec);
+cofunc void getTime(struct tm *time_pointer);
+int validateTime();
+cofunc void getDate(struct tm *time_pointer);
+int validateDate();
+void setClock(unsigned long time_in_sec);
 
 // ---------------------------------- EVENT ----------------------------------
-cofunc void handleCommandInput(int* command_int);
-cofunc void handleParamInput(int* param_int);
-cofunc void createEvent(int command, int params, unsigned long time);
 cofunc void createEventUi();
-cofunc void printEvents();
+cofunc void createEvent(int leds, int info, unsigned long time);
 void insertEvent(struct Event event);
+cofunc void getEventName(int* name_int);
+cofunc void getEventLeds(int* leds_int);
+void printCharInbin(char data);
+cofunc void printEvents();
+int validateEventLeds(char leds);
 
 // Main loop function
 void main () {
@@ -101,14 +107,10 @@ void main () {
 
 // ---------------------------------- SYSTEM ----------------------------------
 
-void initEvents(){
-    int i;
-    struct Event event;
-
-    for (i = 0; i < MAX_NUMBER_EVENTS; i++){
-        event.id = -1;
-        events[i] = event;
-    }
+void init() {
+    MAX_NUMBER_EVENTS = 7;
+    initEvents();
+    setState(MENU);
 }
 
 void setState(enum STATE newState){
@@ -119,11 +121,16 @@ enum STATE getState(){
     return current_state;
 }
 
-void init() {
-    MAX_NUMBER_EVENTS = 5;
-    initEvents();
-    setState(MENU);
+void initEvents(){
+    int i;
+    struct Event event;
+
+    for (i = 0; i < MAX_NUMBER_EVENTS; i++){
+        event.id = -1;
+        events[i] = event;
+    }
 }
+
 // ---------------------------------- MENU ----------------------------------
 
 void menuUI() {
@@ -282,6 +289,7 @@ void inputHourUI() {
 }
 
 cofunc void setDate() {
+    unsigned long time_in_sec;
 
     char data[4];
 
@@ -291,15 +299,15 @@ cofunc void setDate() {
 
     switch(data[0]) {
         case 1:
-            wfd askTimeHourData(1,0);
+            wfd askTimeHourData(&time_in_sec, 1, 0);
             break;
 
         case 2:
-            wfd askTimeHourData(0,1);
+            wfd askTimeHourData(&time_in_sec, 0, 1);
             break;
 
         case 3:
-            wfd askTimeHourData(1,1);
+            wfd askTimeHourData(&time_in_sec, 1, 1);
             break;
 
         case 4:
@@ -313,170 +321,33 @@ cofunc void setDate() {
             printf("Por favor seleccione una de las opciones posibles\n\n");
             abort;
     }
+
+    setClock(time_in_sec);
 }
 
-cofunc void askTimeHourData(int ask_time, int ask_date) {
-    struct tm time;
-    struct tm *time_pointer;
-    unsigned long time_in_sec;
-    char day[10];
-	char month[10];
-	char year[10];
+cofunc void askTimeHourData(unsigned long *time_in_sec, int ask_time, int ask_date) {
+    struct tm time, *time_pointer;
+    //int validate, *validate_pointer;
+    
+    time_pointer = &time;
+
+    printf("\n");
+
+    if (ask_time) {
+        wfd getTime(time_pointer);
+    }
+    if (ask_date) {
+        wfd getDate(time_pointer);
+    }
+
+    *time_in_sec = mktime(time_pointer);
+}
+
+cofunc void getTime(struct tm *time_pointer) {
     char hour[10];
 	char min[10];
 	char sec[10];
-    int day_int;
-    int month_int;
-    int year_int;
-    int hour_int;
-    int min_int;
-    int sec_int;
-    int validate;
-
-    time_pointer = &time;
-
-    printf("\n");
-
-    if (ask_date) {
-        validate = 1;
-
-        // Ask for date
-        printf("Ingrese dia: ");
-        waitfor(getswf(day));
-        printf("Ingrese mes: ");
-        waitfor(getswf(month));
-        printf("Ingrese anio: ");
-        waitfor(getswf(year));
-
-        // Convert strings to int
-        day_int = converter(day);
-        month_int = converter(month);
-        year_int = converter(year);
-
-        if (day_int < 0 | day_int > 31) {
-            validate = 0;
-        }
-        if (month_int < 0 | month_int > 12) {
-            validate = 0;
-        }
-        if (year_int < 1900) {
-            validate = 0;
-        }
-
-        if (validate) {
-            time.tm_mday = day_int;
-            time.tm_mon = month_int;
-            time.tm_year = year_int - 1900;
-        } else {
-        	   CLEAR_SCREEN();
-            printf ("Datos de fecha invalidos\n\n");
-            abort;
-        }
-    }
-
-    if (ask_time) {
-        validate = 1;
-
-        // Ask for time
-        printf("Ingrese hora: ");
-        waitfor(getswf(hour));
-        printf("Ingrese minutos: ");
-        waitfor(getswf(min));
-        printf("Ingrese segundos: ");
-        waitfor(getswf(sec));
-
-        // Convert strings to int
-        hour_int = converter(hour);
-        min_int = converter(min);
-        sec_int = converter(sec);
-
-        if (hour_int < 0 | hour_int > 23) {
-            validate = 0;
-        }
-        if (min_int < 0 | min_int > 59) {
-            validate = 0;
-        }
-        if (sec_int < 0 | sec_int > 59) {
-            validate = 0;
-        }
-
-        if (validate) {
-            time.tm_hour = hour_int;
-            time.tm_min = min_int;
-            time.tm_sec = sec_int;
-        } else {
-        	CLEAR_SCREEN();
-            printf ("Datos de hora invalidos\n\n");
-            abort;
-        }
-    }
-
-    time_in_sec = mktime(time_pointer);
-
-    // Set the RTC time
-    write_rtc(time_in_sec);
-
-    // Update MS_TIMER and SEC_TIMER
-    MS_TIMER = time_in_sec*1000;         // Is in miliseconds
-    SEC_TIMER = time_in_sec;
-    CLEAR_SCREEN();
-}
-
-cofunc void askForDateTime(unsigned long* time_in_sec){
-    struct tm time;
-    struct tm *time_pointer;
-    char day[10];
-	 char month[10];
-	 char year[10];
-    char hour[10];
-	 char min[10];
-	 char sec[10];
-    int day_int;
-    int month_int;
-    int year_int;
-    int hour_int;
-    int min_int;
-    int sec_int;
-    int validate;
-
-    time_pointer = &time;
-
-    printf("\n");
-
-    validate = 1;
-
-    // Ask for date
-    printf("Ingrese dia: ");
-    waitfor(getswf(day));
-    printf("Ingrese mes: ");
-    waitfor(getswf(month));
-    printf("Ingrese anio: ");
-    waitfor(getswf(year));
-
-    // Convert strings to int
-    day_int = converter(day);
-    month_int = converter(month);
-    year_int = converter(year);
-
-    if (day_int < 0 | day_int > 31) {
-        validate = 0;
-    }
-    if (month_int < 0 | month_int > 12) {
-        validate = 0;
-    }
-    if (year_int < 1900) {
-        validate = 0;
-    }
-
-    if (validate) {
-        time.tm_mday = day_int;
-        time.tm_mon = month_int;
-        time.tm_year = year_int - 1900;
-    } else {
-            CLEAR_SCREEN();
-        printf ("Datos de fecha invalidos\n\n");
-        abort;
-    }
+    int hour_int, min_int, sec_int, validate;
 
     validate = 1;
 
@@ -493,6 +364,22 @@ cofunc void askForDateTime(unsigned long* time_in_sec){
     min_int = converter(min);
     sec_int = converter(sec);
 
+    if (validateTime()) {
+        (&(*time_pointer)).tm_hour = hour_int;
+        (&(*time_pointer)).tm_min = min_int;
+        (&(*time_pointer)).tm_sec = sec_int;
+    } else {
+        CLEAR_SCREEN();
+        printf ("Datos de hora invalidos\n\n");
+        abort;
+    }
+}
+
+int validateTime() {
+    int validate;
+
+    validate = 1;
+
     if (hour_int < 0 | hour_int > 23) {
         validate = 0;
     }
@@ -503,119 +390,111 @@ cofunc void askForDateTime(unsigned long* time_in_sec){
         validate = 0;
     }
 
-    if (validate) {
-        time.tm_hour = hour_int;
-        time.tm_min = min_int;
-        time.tm_sec = sec_int;
+    return validate;
+}
+
+cofunc void getDate(struct tm *time_pointer) {
+    char day[10];
+	char month[10];
+	char year[10];
+    int day_int, month_int, year_int, validate;
+
+    validate = 1;
+
+    // Ask for date
+    printf("Ingrese dia: ");
+    waitfor(getswf(day));
+    printf("Ingrese mes: ");
+    waitfor(getswf(month));
+    printf("Ingrese anio: ");
+    waitfor(getswf(year));
+
+    // Convert strings to int
+    day_int = converter(day);
+    month_int = converter(month);
+    year_int = converter(year);
+
+    if (validateDate()) {
+        (&(*time_pointer)).tm_mday = day_int;
+        (&(*time_pointer)).tm_mon = month_int;
+        (&(*time_pointer)).tm_year = year_int - 1900;
     } else {
-        CLEAR_SCREEN();
-        printf ("Datos de hora invalidos\n\n");
+            CLEAR_SCREEN();
+        printf ("Datos de fecha invalidos\n\n");
         abort;
     }
+}
 
+int validateDate() {
+    int validate;
+
+    validate = 1;
+
+    if (day_int < 0 | day_int > 31) {
+            validate = 0;
+        }
+        if (month_int < 0 | month_int > 12) {
+            validate = 0;
+        }
+        if (year_int < 1900) {
+            validate = 0;
+        }
+
+    return validate;
+}
+
+void setClock(unsigned long time_in_sec) {
+    // Set the RTC time
+    write_rtc(time_in_sec);
+
+    
     *time_in_sec = mktime(time_pointer);
+
+    // Update MS_TIMER and SEC_TIMER
+    MS_TIMER = time_in_sec*1000;         // Is in miliseconds
+    SEC_TIMER = time_in_sec;
+    CLEAR_SCREEN();
 }
 
 // ---------------------------------- EVENT ----------------------------------
 
-cofunc void handleCommandInput(int* command_int){
-    char command[4];
-
-    printf("Por favor seleccione uno de los siguientes comandos:\n\n");
-    printf("0 - Prender leds\n\n");
-    // add more actions here if needed
-
-    printf("Seleccione una opcion: ");
-
-    // get command, should be only 8 bits plus EOS
-    waitfor(getswf(command));
-    *command_int = converter(command);
-}
-
-cofunc void handleParamInput(int* param_int){
-    char param[12];
-
-    // print message for user ux
-    printf("Por favor ingrese el parametro a ser usado en la ejecucion del comando:\n\n");
-    printf("-- Debe ser una cadena de 0 o 1 de largo maximo 8 --\n\n");
-
-    waitfor(getswf(param));
-
-    *param_int = converterBin(param);
-}
-
-cofunc void createEvent(int command, int params, unsigned long time){
-    struct Event event;
-
-    event.command = command;
-    event.param = params;
-    event.time = time;
-    insertEvent(event);
-}
-
-cofunc void createEventUi(){
-    int command;
-    int params;
-    unsigned long time;
+cofunc void createEventUi() {
+    char name, leds;
+    unsigned long time_in_sec;
 
     CLEAR_SCREEN();
-    wfd handleCommandInput(&command);
+
+    // Ask for a event name
+    wfd getEventName(&name);
+    // TODO verify
+
+    // Ask for leds value
+    wfd getEventLeds(&leds);
     // check if entered value belongs to the possible values
-    if (!(command == 0)) {
-         // if not in options, abort
+    if (!validateEventLeds(leds)) {
         CLEAR_SCREEN();
-        printf("Error: por favor ingrese un commando valido\n\n");
+        printf("Error: por favor ingrese una valida\n\n");
         abort;
     }
-    CLEAR_SCREEN();
-    wfd handleParamInput(&params);
-    // validate if only 1 and o
-    CLEAR_SCREEN();
-    wfd askForDateTime(&time);
-    CLEAR_SCREEN();
-    printf("Creating event....");
-    wfd createEvent(command, params, time);
-    printf("OK");
-    yield;
-    CLEAR_SCREEN();
+
+    // Ask for time and date
+    printf("\n\n");
+    wfd askTimeHourData(&time_int_sec, 1, 1);
+
+    // Create the event
+    wfd createEvent(name, leds, time_in_sec);
+
+    printf("Su evento fue agregado a la lista de eventos programados\n\n");
     setState(MENU);
 }
 
-// Given a char print it as binary
-void printCharInbin(char data){
-    char a;
-    int i;
-
-    a = data;
-    for (i = 0; i < 8; i++) {
-        printf("%d", !!((a << i) & 0x80));
-    }
-    printf("\n\n");
-}
-
-
-cofunc void printEvents(){
+cofunc void createEvent(char name, char leds, unsigned long time) {
     struct Event event;
-    int i;
 
-    CLEAR_SCREEN();
-
-    printf("Listado de eventos:\n\n");
-
-    for (i = 0; i < MAX_NUMBER_EVENTS; i++){
-        event = events[i];
-
-        if (event.id != -1) {
-            printf("Comando de evento: Prender leds\n\n");
-            printf("Parametro de evento: ");
-            printCharInbin(event.param);
-            displayHourUI(event.time);
-            printf("==============<>==============\n\n");
-        }
-
-    }
-
-    printf("\n\n");
+    event.name = name;
+    event.leds = leds;
+    event.time = time;
+    insertEvent(event);
 }
 
 void insertEvent(struct Event event) {
@@ -635,4 +514,93 @@ void insertEvent(struct Event event) {
     if (!inserted) {
         printf("No hay espacio para mas eventos");
     }
+}
+
+cofunc void getEventName(int* name_int) {
+    char name[4];
+
+    printf("Por favor ingrese un nombre para el evento: ");
+
+    // get name
+    waitfor(getswf(name));
+    *name_int = converter(name);
+}
+
+cofunc void getEventLeds(int* leds_int) {
+    char leds[12];
+
+    // print message for user ux
+    printf("Por favor ingrese una cadena de largo 8 de 0 o 1 para la salida de los leds: \n\n");
+
+    waitfor(getswf(leds));
+
+    *leds_int = converterBin(leds);
+}
+
+// Given a char print it as binary
+void printCharInbin(char data) {
+    char a, i;
+
+    a = data;
+    for (i = 0; i < 8; i++) {
+        printf("%d", !!((a << i) & 0x80));
+    }
+    printf("\n\n");
+}
+
+
+cofunc void printEvents() {
+    struct Event event;
+    int i;
+
+    CLEAR_SCREEN();
+
+    printf("Listado de eventos:\n\n");
+
+    for (i = 0; i < MAX_NUMBER_EVENTS; i++){
+        event = events[i];
+
+        if (event.id != -1) {
+            printf("*************** %d ***************\n\n", i);
+            printf("Nombre: %c\n\n", event.name);
+            printf("Leds: ");
+            printCharInbin(event.param);
+            displayHourUI(event.time);
+        }
+    }
+
+    printf("\n\n");
+}
+
+int validateEventLeds(char leds) {
+    int i, length, result;
+
+    i = 0;
+    length = 0;
+    result = 1;
+
+    // get the length of the string
+    while(*(data+i) != '\0'){
+        length++;
+        i++;
+    }
+
+    if (length > 8) {
+        return 0;
+    }
+    i = 0;
+
+    while (*(data+i) != '\0') {
+
+        // get value of string
+        *(data + i) = *(data + i) - 0x30;       //0x30 hex = 48 ascii = 0
+
+        if (*(data + i != 1 | *(data + i != 0)) {
+            return 0;
+        }
+
+        i++;
+    }
+
+    return result;
 }
