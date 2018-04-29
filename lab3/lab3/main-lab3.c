50b02d097043b009bd6dfdfabf6a3276093cf108
GabrielMaz <gabrielmaz114@gmail.com>
2018-04-28 23:55:50 -0300

    Add option selected

--- lab3/lab3/main-lab3.c
+++ lab3/lab3/main-lab3.c
@@ -4,8 +4,16 @@
 #use LAB3_EVENT.LIB
 #use LAB3_SYSTEM.LIB
 #use LAB3_PIC.LIB
+#use LAB3_ETHERNET.LIB
 
 void main () {
+    char buffer[2048];
+	int status;
+	
+	printf("Iniciando Socket\n");
+	sock_init();
+	printf("Iniciado\n");
+
     init();
 
     current_state = getState();
@@ -13,62 +21,9 @@ void main () {
     while (1) {
 
         costate {
-            switch(current_state){
-                case MENU:
-                    menuUI();
-                    wfd optionSelected();
-                    break;
-
-                case DISPLAY_HOUR:
-                    displayHourUI(getRtcTime());
-                    setState(MENU);
-                    break;
-
-                case INPUT_HOUR:
-                    inputHourUI();
-                    wfd setDate();
-                    break;
-
-                case LIST_EVENTS:
-                    wfd printEvents();
-                    setState(MENU);
-                    break;
-
-                case ADD_EVENT:
-                    if (events_actived < MAX_NUMBER_EVENTS) {
-                        wfd createEventUi();
-                    } else {
-                        CLEAR_SCREEN();
-                        printf("No hay espacio para mas eventos\n\n");
-                    }
-                    setState(MENU);
-                    break;
-
-                case DELETE_EVENT:
-                    wfd printEvents();
-                    if (events_actived > 0) {
-                        wfd deleteEventUI();
-                    } else {
-                        CLEAR_SCREEN();
-                        printf("No hay eventos para eliminar\n\n");
-                    }
-                    setState(MENU);
-                    break;
-                
-                case ANALOG_INPUT_0:
-                    CLEAR_SCREEN();
-                    getAnalogInput(0);
-			        delayMS(PIC_TIMEOUT);
-                    setState(MENU);
-                    break;
-
-                case ANALOG_INPUT_1:
-                    CLEAR_SCREEN();
-                    getAnalogInput(1);
-			        delayMS(PIC_TIMEOUT);
-                    setState(MENU);
-                    break;
-            }
+        
+            wfd selectOption(current_state);
+        
         }
     }
 }
