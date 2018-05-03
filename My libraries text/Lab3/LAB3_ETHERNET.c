dda3494b5575cbf7e4e11d2b9f5f83adbe2af8f4
GabrielMaz <gabrielmaz114@gmail.com>
2018-05-03 17:19:17 -0300

    Add Ethernet functions

---
+++ My libraries text/Lab3/LAB3_ETHERNET.c
@@ -0,0 +1,69 @@
+/*** BeginHeader */
+
+#define TCPCONFIG 0
+
+#define USE_ETHERNET		1
+#define MY_IP_ADDRESS "10.10.6.100"
+#define MY_NETMASK "255.255.255.0"
+#define MY_GATEWAY "10.10.6.2"
+
+#define CONNECTION_DIM 50
+
+#define CLEAR_BUFFER() (memset(&buffer, '\0', 2048));
+
+#memmap xmem
+#use "dcrtcp.lib"
+
+#define PORT 7
+
+tcp_Socket socket;
+char buffer[2048];
+int status;
+
+/*** EndHeader */
+
+
+/*** BeginHeader initSocket*/
+void initSocket();
+/*** EndHeader */
+
+void initSocket() {
+    printf("Iniciando Socket\n");
+	sock_init();
+	printf("Iniciado\n");
+
+    tcp_listen(&socket,PORT,0,0,NULL,0);
+    sock_wait_established(&socket,sock_delay,NULL,&status)
+    printf("Conectado\n");
+    sock_mode(&socket,TCP_MODE_ASCII);
+
+    sock_err:
+    switch(status) {
+        case 1: /* foreign host closed */
+            printf("User closed session\n");
+            break;
+        case -1: /* time-out */
+            printf("Connection timed out\n");
+            break;
+    }
+}
+
+/*** BeginHeader cleanScreenEthernet */
+void cleanScreenEthernet();
+/*** EndHeader */
+
+void cleanScreenEthernet() {
+    sprintf(buffer, "\n\n");
+    sock_puts(&socket,buffer);
+    CLEAR_BUFFER();
+}
+
+/*** BeginHeader printEthernet */
+void printEthernet();
+/*** EndHeader */
+
+void printEthernet(char *message) {
+    sprintf(buffer, message);
+    sock_puts(&socket,buffer);
+    CLEAR_BUFFER();
+}
